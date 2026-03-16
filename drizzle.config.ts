14276ce73dfb64027c1f7f2d53c4f54564dae913
Igor Mishin <igorigor6142@gmail.com>
2026-03-16 14:50:32 +0500

    Fix: switch to neon serv. driver

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from 'drizzle-kit'
+import { config } from 'dotenv'
+config({ path: '.env.local' })
 
 export default {
   schema: './src/lib/db/schema.ts',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.POSTGRES_URL || '',
+    url: process.env.POSTGRES_URL || process.env.DATABASE_URL || '',
   },
 } satisfies Config
